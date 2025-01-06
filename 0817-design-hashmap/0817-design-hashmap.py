7c6a82b688bdf96dba6b195af959242558e8b8a4
Karl <93551434+Karl-Fernandes@users.noreply.github.com>
2025-01-06 11:38:25 +0000

    Time: 89 ms (49.18%), Space: 23.2 MB (27.96%) - LeetHub

--- 0817-design-hashmap/0817-design-hashmap.py
+++ 0817-design-hashmap/0817-design-hashmap.py
@@ -2,11 +2,10 @@ class MyHashMap(object):
 
     def __init__(self):
         self.size = 10000
-        self.table ={i: [] for i in range(self.size)}
+        self.table = {i: {} for i in range(self.size)}
 
     def calculateHashValue(self, key):
         return key % self.size
-        
 
     def put(self, key, value):
         """
@@ -15,19 +14,7 @@ class MyHashMap(object):
         :rtype: None
         """
         hv = self.calculateHashValue(key)
-
-
-        if self.table[hv] != None:
-            for idx, (existing_key, _) in enumerate(self.table[hv]):
-                if existing_key == key:
-                    self.table[hv][idx] = (key, value)
-                    return
-            self.table[hv].append((key, value))
-        else:
-            self.table[hv].append((key, value))
-                
-
-
+        self.table[hv][key] = value
 
     def get(self, key):
         """
@@ -35,12 +22,9 @@ class MyHashMap(object):
         :rtype: int
         """
         hv = self.calculateHashValue(key)
-
-        for element in self.table[hv]:
-            if element[0] == key:
-                return element[1]
-        return -1
-        
+        if key in self.table[hv]:
+            return self.table[hv][key]
+        return -1  
 
     def remove(self, key):
         """
@@ -48,18 +32,5 @@ class MyHashMap(object):
         :rtype: None
         """
         hv = self.calculateHashValue(key)
-
-        for idx, (existing_key, _) in enumerate(self.table[hv]):
-            if existing_key == key:
-                self.table[hv].pop(idx) 
-                return
-        
-
-
-
-
-# Your MyHashMap object will be instantiated and called as such:
-# obj = MyHashMap()
-# obj.put(key,value)
-# param_2 = obj.get(key)
-# obj.remove(key)
+        if key in self.table[hv]:
+            del self.table[hv][key]
