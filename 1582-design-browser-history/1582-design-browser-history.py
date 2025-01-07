eeb2f01cff10e91a1eb54847452800646dead4ae
Karl <93551434+Karl-Fernandes@users.noreply.github.com>
2025-01-07 18:16:27 +0000

    Time: 53 ms (26.77%), Space: 20.1 MB (21.35%) - LeetHub

--- 1582-design-browser-history/1582-design-browser-history.py
+++ 1582-design-browser-history/1582-design-browser-history.py
@@ -5,51 +5,21 @@ class BrowserHistory:
         self.back_history = []
         self.front_history = []
 
-        
-        
-
     def visit(self, url: str) -> None:
         self.back_history.append(self.current)
-        self.front_history = []
+        self.front_history = []  
         self.current = url
 
-        
-
     def back(self, steps: int) -> str:
-        if steps <= len(self.back_history):
-            for step in range(steps):
-                temp = self.current
-                self.current = self.back_history.pop()
-                self.front_history.append(temp)
-        else:
-            for step in range(len(self.back_history)):
-                temp = self.current
-                self.current = self.back_history.pop()
-                self.front_history.append(temp)
-        
+        steps = min(steps, len(self.back_history))
+        for _ in range(steps):
+            self.front_history.append(self.current)
+            self.current = self.back_history.pop()
         return self.current
 
     def forward(self, steps: int) -> str:
-        if steps <= len(self.front_history):
-            for step in range(steps):
-                temp = self.current
-                self.current = self.front_history.pop()
-                self.back_history.append(temp)
-        else:
-            for step in range(len(self.front_history)):
-                temp = self.current
-                self.current = self.front_history.pop()
-                self.back_history.append(temp)
-
-        
+        steps = min(steps, len(self.front_history))
+        for _ in range(steps):
+            self.back_history.append(self.current)
+            self.current = self.front_history.pop()
         return self.current
- 
-
-        
-
-
-# Your BrowserHistory object will be instantiated and called as such:
-# obj = BrowserHistory(homepage)
-# obj.visit(url)
-# param_2 = obj.back(steps)
-# param_3 = obj.forward(steps)
