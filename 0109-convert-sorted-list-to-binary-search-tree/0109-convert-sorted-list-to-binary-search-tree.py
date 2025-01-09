2368c6c05b0e8fd36cc5fb22aaa774c9a5cd0d84
Karl <93551434+Karl-Fernandes@users.noreply.github.com>
2025-01-09 22:49:23 +0000

    Time: 6 ms (78.73%), Space: 20.3 MB (73.42%) - LeetHub

--- 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.py
+++ 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.py
@@ -4,6 +4,7 @@ class Solution:
             return None
         if not head.next:
             return TreeNode(head.val)
+            
         middle = self.getMiddle(head)
         root = TreeNode(middle.val)
         root.right = self.sortedListToBST(middle.next)
@@ -15,10 +16,13 @@ class Solution:
         fast = head
         slow = head
         prev = None
+
         while fast and fast.next:
             fast = fast.next.next
             prev = slow
             slow = slow.next
+
         if prev:
             prev.next = None
+            
         return slow
