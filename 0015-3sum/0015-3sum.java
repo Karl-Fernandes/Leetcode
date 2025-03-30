307c2a4e71ca0f3c9bffe0b4bf83cb8606fcdd45
Karl <93551434+Karl-Fernandes@users.noreply.github.com>
2025-03-30 13:13:36 +0100

    Time: 31 ms (58.5%), Space: 51.9 MB (39.46%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,9 +1,3 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.HashSet;
-import java.util.Set;
-
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
@@ -11,7 +5,7 @@ class Solution {
         
         for (int i = 0; i < nums.length - 2; i++) {
             if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
+                continue; // Skip duplicates
             }
             
             int newTarget = -nums[i];
@@ -26,17 +20,22 @@ class Solution {
 
     private List<int[]> twoSum(int[] nums, int start, int target) {
         List<int[]> pairs = new ArrayList<>();
-        Set<Integer> seen = new HashSet<>();
-        
-        for (int j = start; j < nums.length; j++) {
-            int complement = target - nums[j];
-            if (seen.contains(complement)) {
-                pairs.add(new int[]{complement, nums[j]});
-                while (j + 1 < nums.length && nums[j] == nums[j + 1]) {
-                    j++;
-                }
+        int left = start;
+        int right = nums.length - 1;
+
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+            if (sum == target) {
+                pairs.add(new int[]{nums[left], nums[right]});
+                while (left < right && nums[left] == nums[left + 1]) left++;
+                while (left < right && nums[right] == nums[right - 1]) right--;
+                left++;
+                right--;
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
             }
-            seen.add(nums[j]);
         }
         return pairs;
     }
