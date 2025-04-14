6fba8e8aa66114b9bc6c4c5ee22274c4ad24e822
Karl <93551434+Karl-Fernandes@users.noreply.github.com>
2025-04-14 10:14:28 +0100

    Time: 160 ms (35.07%), Space: 78.2 MB (89.66%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,40 +1,53 @@
-class LRUCache:
+class Node:
+    def __init__(self, key: int, value: int):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
 
+class LRUCache:
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.dict = {}
-        self.access_log = {}
-        self.counter = 0
+        self.cache = {}  # key -> node
 
-        
+        # Dummy head and tail to avoid edge cases
+        self.head = Node(0, 0)
+        self.tail = Node(0, 0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def _remove(self, node):
+        """Remove node from list"""
+        prev_node = node.prev
+        next_node = node.next
+        prev_node.next = next_node
+        next_node.prev = prev_node
+
+    def _add_to_front(self, node):
+        """Add node right after head (most recently used)"""
+        node.next = self.head.next
+        node.prev = self.head
+        self.head.next.prev = node
+        self.head.next = node
 
     def get(self, key: int) -> int:
-        if key in self.dict:
-            self.counter += 1
-            self.access_log[key] = self.counter
-            return self.dict[key]
+        if key in self.cache:
+            node = self.cache[key]
+            self._remove(node)
+            self._add_to_front(node)
+            return node.value
         return -1
-        
 
     def put(self, key: int, value: int) -> None:
-        if key in self.dict:
-            self.dict[key] = value
-        else:
-            if len(self.dict) == self.capacity:
-                old_key = min(self.access_log, key = self.access_log.get)
-                self.dict.pop(old_key)
-                self.access_log.pop(old_key)
- 
-            self.dict[key] = value
-            
-        self.counter += 1
-        self.access_log[key] = self.counter
-            
+        if key in self.cache:
+            self._remove(self.cache[key])
 
-        
+        node = Node(key, value)
+        self._add_to_front(node)
+        self.cache[key] = node
 
-
-# Your LRUCache object will be instantiated and called as such:
-# obj = LRUCache(capacity)
-# param_1 = obj.get(key)
-# obj.put(key,value)
+        if len(self.cache) > self.capacity:
+            # Remove from tail
+            lru = self.tail.prev
+            self._remove(lru)
+            del self.cache[lru.key]
