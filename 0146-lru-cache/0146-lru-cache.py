87e25887da15564b2d8b98e6296436e5e83f185e
Karl <93551434+Karl-Fernandes@users.noreply.github.com>
2025-04-15 10:11:18 +0100

    Time: 192 ms (12.35%), Space: 78.8 MB (13.45%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,53 +1,60 @@
 class Node:
-    def __init__(self, key: int, value: int):
-        self.key = key
-        self.value = value
-        self.prev = None
-        self.next = None
+    def __init__(self, key, value):
+        self.key, self.value = key, value
+        self.prev = self.next = None
 
 class LRUCache:
+
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.cache = {}  # key -> node
+        self.dict = {}
+        
+        self.left, self.right = Node(0,0), Node(0,0)
+        self.left.next, self.right.prev = self.right, self.left
 
-        # Dummy head and tail to avoid edge cases
-        self.head = Node(0, 0)
-        self.tail = Node(0, 0)
-        self.head.next = self.tail
-        self.tail.prev = self.head
+    def insert(self, node):
+        prev, nxt = self.right.prev, self.right
+        prev.next = nxt.prev = node
+        node.prev, node.next = prev, nxt
+         
 
-    def _remove(self, node):
-        """Remove node from list"""
-        prev_node = node.prev
-        next_node = node.next
-        prev_node.next = next_node
-        next_node.prev = prev_node
+    def remove(self, node):
+        prev, nxt = node.prev, node.next
+        prev.next, nxt.prev = nxt, prev
 
-    def _add_to_front(self, node):
-        """Add node right after head (most recently used)"""
-        node.next = self.head.next
-        node.prev = self.head
-        self.head.next.prev = node
-        self.head.next = node
+        
 
     def get(self, key: int) -> int:
-        if key in self.cache:
-            node = self.cache[key]
-            self._remove(node)
-            self._add_to_front(node)
-            return node.value
+        if key in self.dict:
+            self.remove(self.dict[key])
+            self.insert(self.dict[key])
+            return self.dict[key].value
         return -1
+        
 
     def put(self, key: int, value: int) -> None:
-        if key in self.cache:
-            self._remove(self.cache[key])
+        if key in self.dict:
+            self.remove(self.dict[key])
 
-        node = Node(key, value)
-        self._add_to_front(node)
-        self.cache[key] = node
+        self.dict[key] = Node(key, value)
+        self.insert(self.dict[key])
+        
 
-        if len(self.cache) > self.capacity:
-            # Remove from tail
-            lru = self.tail.prev
-            self._remove(lru)
-            del self.cache[lru.key]
+        if len(self.dict) > self.capacity:
+            old_key = self.left.next
+            self.remove(old_key)
+            del self.dict[old_key.key]
+        
+        
+        
+
+        
+            
+
+        
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
