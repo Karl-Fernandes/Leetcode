dfad4dc5b2510ce6fc9199e0e3b98831048257ee
Karl <93551434+Karl-Fernandes@users.noreply.github.com>
2025-07-13 12:03:36 +0100

    Time: 0 ms (100%), Space: 12.6 MB (14.74%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -1,3 +1,8 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution(object):
     def addTwoNumbers(self, l1, l2):
         """
@@ -5,22 +10,32 @@ class Solution(object):
         :type l2: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        dummy_head = ListNode(0)
-        current = dummy_head
+        dummy = ListNode()
+        pointer = dummy
         carry = 0
 
-        while l1 or l2 or carry:
-            val1 = l1.val if l1 else 0
-            val2 = l2.val if l2 else 0
-
-            total = val1 + val2 + carry
-
+        while l1 and l2:
+            total = l1.val + l2.val + carry
+            pointer.next = ListNode(total % 10)
             carry = total // 10
-            current.next = ListNode(total % 10)
-            current = current.next
+            l1, l2 = l1.next, l2.next
+            pointer = pointer.next
 
-            if l1: l1 = l1.next
-            if l2: l2 = l2.next
+        while l1:
+            total = l1.val + carry
+            pointer.next = ListNode(total % 10)
+            carry = total // 10
+            l1, pointer = l1.next, pointer.next
 
-       
-        return dummy_head.next
+        while l2:
+            total = l2.val + carry
+            pointer.next = ListNode(total % 10)
+            carry = total // 10
+            l2, pointer = l2.next, pointer.next    
+
+        if carry:
+            pointer.next = ListNode(carry)
+
+        return dummy.next
+
+
