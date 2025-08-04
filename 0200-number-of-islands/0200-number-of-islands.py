11dd323adf8c7be546870ba16965273e0d1b620e
Karl <93551434+Karl-Fernandes@users.noreply.github.com>
2025-08-04 22:24:46 +0100

    Time: 240 ms (70.27%), Space: 20.1 MB (82.35%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,5 +1,9 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
+
+            
         ROWS, COLS = len(grid), len(grid[0])
         directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
         islands = 0
