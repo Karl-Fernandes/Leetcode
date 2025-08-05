34982dc49afa9e9c3ee68b17fc2ff6201686ac9d
Karl <93551434+Karl-Fernandes@users.noreply.github.com>
2025-08-05 16:47:19 +0100

    Time: 30 ms (71.6%), Space: 31.3 MB (92.83%) - LeetHub

--- 2664-maximize-greatness-of-an-array/2664-maximize-greatness-of-an-array.py
+++ 2664-maximize-greatness-of-an-array/2664-maximize-greatness-of-an-array.py
@@ -1,20 +1,10 @@
 from typing import List
 
 class Solution:
-    def maximizeGreatness(self, nums: List[int]) -> int:
-        sorted_nums = sorted(nums)
-        n = len(nums)
+    def maximizeGreatness(self, A):
+        A.sort()
         res = 0
-        
-        pointer1 = 0  # pointer for original sorted array
-        pointer2 = 0  # pointer for candidate numbers
-        
-        while pointer1 < n and pointer2 < n:
-            if sorted_nums[pointer2] > sorted_nums[pointer1]:
+        for a in A:
+            if a > A[res]:
                 res += 1
-                pointer1 += 1
-                pointer2 += 1
-            else:
-                pointer2 += 1
-        
         return res
