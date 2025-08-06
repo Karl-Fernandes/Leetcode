d9981dcd9f25c7aaf8053e45547e0390297121d1
Karl <93551434+Karl-Fernandes@users.noreply.github.com>
2025-08-06 09:28:50 +0100

    Time: 36 ms (69.82%), Space: 26.1 MB (71.17%) - LeetHub

--- 3307-find-the-maximum-sum-of-node-values/3307-find-the-maximum-sum-of-node-values.py
+++ 3307-find-the-maximum-sum-of-node-values/3307-find-the-maximum-sum-of-node-values.py
@@ -1,11 +1,15 @@
 class Solution:
     def maximumValueSum(self, nums: List[int], k: int, edges: List[List[int]]) -> int:
-        dp = [0, float("-inf")]
+        delta = [(num ^ k) - num for num in nums]
+        delta.sort(reverse=True)
+        res = sum(nums)
 
-        for i in range(len(nums) - 1, -1, -1):
-            next_dp = [0, 0]
-            next_dp[0] = max(nums[i] + dp[0], (nums[i] ^ k) + dp[1])
-            next_dp[1] = max(nums[i] + dp[1], (nums[i] ^ k) + dp[0])
-            dp = next_dp
+        for i in range(0, len(nums), 2):
+            if i == len(nums) - 1:
+                break
+            path_delta = delta[i] + delta[i + 1]
+            if path_delta <= 0:
+                break
+            res += path_delta
 
-        return dp[0]
+        return res
