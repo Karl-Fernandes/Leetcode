0253bb707d58b12fc6ab61374995ee688d6ebc7b
Karl <93551434+Karl-Fernandes@users.noreply.github.com>
2025-08-10 11:25:44 +0100

    Time: 127 ms (76.83%), Space: 17.9 MB (34.85%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -3,7 +3,8 @@ class Solution:
         if not s:
             return 0
 
-        def expand_around_center(s: str, left: int, right: int, count: int) -> int:
+        def expand_around_center(s: str, left: int, right: int) -> int:
+            count = 0
             while left >= 0 and right < len(s) and s[left] == s[right]:
                 left -= 1
                 right += 1
@@ -13,8 +14,8 @@ class Solution:
         total = 0
 
         for i in range(len(s)):
-            total += expand_around_center(s, i, i, 0)
-            total += expand_around_center(s, i, i + 1, 0)
+            total += expand_around_center(s, i, i)
+            total += expand_around_center(s, i, i + 1)
 
            
         return total
