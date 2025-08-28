46531f6fca7f507783442270f9b91e5a24e202d4
Karl <93551434+Karl-Fernandes@users.noreply.github.com>
2025-08-28 18:07:10 +0100

    Time: 5 ms (34.81%), Space: 17.8 MB (44.79%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -1,43 +1,34 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         ROWS, COLS = len(grid), len(grid[0])
-        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        DIRECTIONS = [[1, 0], [-1, 0], [0, 1], [0, -1]]
         fresh = 0
         time = 0
-
         q = deque()
 
         for r in range(ROWS):
             for c in range(COLS):
                 if grid[r][c] == 2:
                     q.append([r, c])
-                elif grid[r][c] == 1:
+                if grid[r][c] == 1:
                     fresh += 1
         
-        def updateCell(r, c):
+        def rotten(r, c):
             if r < 0 or c < 0 or r >= ROWS or c >= COLS or grid[r][c] != 1:
-                return
-
-            nonlocal fresh
+                return False
+            
             grid[r][c] = 2
-            fresh -= 1
             q.append([r, c])
+            return True
         
-        while fresh > 0 and q:
+
+        while q and fresh:
             for _ in range(len(q)):
                 r, c = q.popleft()
-
-                for dr, dc in directions:
-                    updateCell(r + dr, c + dc)
+                for dr, dc in DIRECTIONS:
+                    if rotten(r + dr, c + dc):
+                        fresh -= 1
             time += 1
-    
-        return time if fresh == 0 else -1
-
-
-       
         
-
-
-
-
+        return time if not fresh else -1
         
