f33831de4ddceb2b079a39bd0a18031ceb368fa6
Karl <93551434+Karl-Fernandes@users.noreply.github.com>
2025-09-02 19:18:19 +0100

    Time: 191 ms (11.71%), Space: 78 MB (43.49%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,52 +1,51 @@
 class Node:
-    def __init__(self, key, value):
+    def __init__(self, value=0, key=0):
         self.key, self.value = key, value
         self.prev = self.next = None
 
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.capacity = capacity
+        self.capacity = capacity 
         self.dict = {}
+
+        self.head = Node()
+        self.tail = Node()
+
+        self.head.next, self.tail.prev = self.tail, self.head
         
-        self.left, self.right = Node(0,0), Node(0,0)
-        self.left.next, self.right.prev = self.right, self.left
-
-    def insert(self, node):
-        prev, nxt = self.right.prev, self.right
-        prev.next = nxt.prev = node
-        node.prev, node.next = prev, nxt
-         
-
-    def remove(self, node):
+    def _remove(self, node: Node):
         prev, nxt = node.prev, node.next
         prev.next, nxt.prev = nxt, prev
-
-        
+    
+    def _insert(self, node: Node):
+        prev, nxt = self.head, self.head.next
+        prev.next = nxt.prev = node
+        node.next, node.prev = nxt, prev
 
     def get(self, key: int) -> int:
         if key in self.dict:
-            self.remove(self.dict[key])
-            self.insert(self.dict[key])
+            self._remove(self.dict[key])
+            self._insert(self.dict[key])
             return self.dict[key].value
         return -1
-        
 
     def put(self, key: int, value: int) -> None:
         if key in self.dict:
-            self.remove(self.dict[key])
-
-        self.dict[key] = Node(key, value)
-        self.insert(self.dict[key])
+            self._remove(self.dict[key])
         
+        self.dict[key] = Node(value, key)
+        self._insert(self.dict[key])
 
         if len(self.dict) > self.capacity:
-            old_key = self.left.next
-            self.remove(old_key)
+            old_key = self.tail.prev
+            self._remove(old_key)
             del self.dict[old_key.key]
-        
-        
-        
 
         
-            
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
