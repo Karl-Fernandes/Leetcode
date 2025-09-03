24d718af0bbda8c0464bdb29ed7b96e5e8fd937d
Karl <93551434+Karl-Fernandes@users.noreply.github.com>
2025-09-03 19:49:14 +0100

    Time: 0 ms (100%), Space: 18.2 MB (8.39%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,24 +1,24 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
         memo = {}
-        
+
         def dfs(i):
             if i == len(s):
-                return 1  # reached end, valid decode
+                return 1
+            
             if s[i] == '0':
-                return 0  # no decode starts with 0
+                return 0
             
             if i in memo:
                 return memo[i]
+
+            memo[i] = dfs(i + 1)
+
+            if i + 1 < len(s) and int(s[i: i + 2]) <= 26:
+                memo[i] += dfs(i + 2)
             
-            # Decode one digit
-            ways = dfs(i + 1)
-            
-            # Decode two digits, if valid
-            if (i + 1 < len(s)) and (int(s[i:i+2]) <= 26):
-                ways += dfs(i + 2)
-            
-            memo[i] = ways
-            return ways
+            return memo[i]
         
         return dfs(0)
+            
+
