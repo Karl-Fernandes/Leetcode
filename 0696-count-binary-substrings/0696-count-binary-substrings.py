e1817ebb84137db5eecf655e07f30eef183e2991
Karl <93551434+Karl-Fernandes@users.noreply.github.com>
2025-09-03 22:24:07 +0100

    Time: 75 ms (50.6%), Space: 18.2 MB (47.01%) - LeetHub

--- 0696-count-binary-substrings/0696-count-binary-substrings.py
+++ 0696-count-binary-substrings/0696-count-binary-substrings.py
@@ -1,18 +1,16 @@
 class Solution:
     def countBinarySubstrings(self, s: str) -> int:
+        current = 1
+        prev = 0
+        res = 0
 
-        def partitionMiddle(l, r, c):
-            while l >= 0 and r < len(s) and s[l] == s[l + 1] and s[r] == s[r - 1]:
-                r += 1
-                l -= 1
-                c += 1
-            return c
-                    
-
-
-        total = 0
-        for i in range(len(s) - 1):
-            if s[i] != s[i + 1]:
-                total += partitionMiddle(i - 1, i + 2, 1)
+        for i in range(1, len(s)):
+            if s[i] == s[i - 1]:
+                current += 1
+            else:
+                res += min(current, prev)
+                prev = current
+                current = 1
+        res += min(current, prev)
         
-        return total
+        return res
