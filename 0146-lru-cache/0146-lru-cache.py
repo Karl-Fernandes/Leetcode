ea415eba9fe2b1afa1e2f2ebc50f35f555e47d8e
Karl <93551434+Karl-Fernandes@users.noreply.github.com>
2025-09-04 16:56:48 +0100

    Time: 173 ms (21.2%), Space: 78.1 MB (43.49%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,5 +1,5 @@
 class Node:
-    def __init__(self, value=0, key=0):
+    def __init__(self, key=0, value=0):
         self.key, self.value = key, value
         self.prev = self.next = None
 
@@ -8,41 +8,39 @@ class LRUCache:
     def __init__(self, capacity: int):
         self.capacity = capacity 
         self.dict = {}
-
-        self.head = Node()
-        self.tail = Node()
-
-        self.head.next, self.tail.prev = self.tail, self.head
         
-    def _remove(self, node: Node):
-        prev, nxt = node.prev, node.next
-        prev.next, nxt.prev = nxt, prev
-    
-    def _insert(self, node: Node):
-        prev, nxt = self.head, self.head.next
-        prev.next = nxt.prev = node
-        node.next, node.prev = nxt, prev
+        self.head, self.tail = Node(), Node()
+        self.head.next, self.tail.prev = self.tail, self.head
 
+    def _remove(self, node: Node):
+        prv, nxt = node.prev, node.next
+        prv.next, nxt.prev = nxt, prv 
+
+    def _insert(self, node: Node):
+        prv, nxt = self.head, self.head.next
+        prv.next = nxt.prev = node
+        node.prev, node.next = prv, nxt
+    
     def get(self, key: int) -> int:
         if key in self.dict:
             self._remove(self.dict[key])
             self._insert(self.dict[key])
             return self.dict[key].value
+        
         return -1
 
     def put(self, key: int, value: int) -> None:
         if key in self.dict:
             self._remove(self.dict[key])
         
-        self.dict[key] = Node(value, key)
+        self.dict[key] = Node(key, value)
         self._insert(self.dict[key])
 
         if len(self.dict) > self.capacity:
-            old_key = self.tail.prev
-            self._remove(old_key)
-            del self.dict[old_key.key]
+            oldest = self.tail.prev
+            self._remove(oldest)
+            del self.dict[oldest.key]
 
-        
 
 
 # Your LRUCache object will be instantiated and called as such:
