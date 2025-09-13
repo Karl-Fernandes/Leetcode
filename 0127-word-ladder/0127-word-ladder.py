fa0cf8bd035c7ca778c7261631bbab1596abcaac
Karl <93551434+Karl-Fernandes@users.noreply.github.com>
2025-09-13 13:57:50 +0100

    Time: 48 ms (93.24%), Space: 20.8 MB (40.17%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -9,17 +9,17 @@ class Solution:
             for j in range(len(word)):
                 pattern = word[:j] + "*" + word[j + 1:]
                 nei[pattern].append(word)
-        
 
         visited = set([beginWord])
         q = deque([beginWord])
         res = 1
 
         while q:
-            for i in range(len(q)):
+            for _ in range(len(q)): 
                 current = q.popleft()
                 if current == endWord:
                     return res
+
                 for j in range(len(current)):
                     pattern = current[:j] + "*" + current[j + 1:]
                     for neiWord in nei[pattern]:
