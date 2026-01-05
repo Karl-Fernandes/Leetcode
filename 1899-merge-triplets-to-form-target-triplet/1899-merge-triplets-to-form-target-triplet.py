0ebda2a2f6064a59446bb783eed03628985ab43a
Karl <93551434+Karl-Fernandes@users.noreply.github.com>
2026-01-05 10:31:52 +0000

    Time: 55 ms (13.93%), Space: 51.8 MB (77.42%) - LeetHub

--- 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.py
+++ 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.py
@@ -3,7 +3,8 @@ class Solution:
         res = [0, 0, 0]
         for t in triplets:
             if t[0] <= target[0] and t[1] <= target[1] and t[2] <= target[2]:
-                res = [max(x, y) for x, y in zip(t, res)]
+                for i in range(3):
+                    res[i] = max(res[i], t[i])
         
         return all((x == y) for x, y in zip(res, target))
                                           
