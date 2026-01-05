4d6d29d5923a857b5874b6bd8ed84ca942f914e3
Karl <93551434+Karl-Fernandes@users.noreply.github.com>
2026-01-05 13:40:10 +0000

    Time: 3 ms (65.17%), Space: 19.2 MB (81.77%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,12 +1,6 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        visited = {}
-
+        sum = 0
         for num in nums:
-            if num in visited:
-                visited[num] += 1
-            else:
-                visited[num] = 1
-        
-        print(visited)
-        return min(visited, key=visited.get)
+            sum ^= num     
+        return sum
