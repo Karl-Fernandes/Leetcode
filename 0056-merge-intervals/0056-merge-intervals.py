553ada39ef27e0e9106159c4768b92d1f6d17227
Karl <93551434+Karl-Fernandes@users.noreply.github.com>
2026-01-21 15:00:21 +0000

    Time: 3 ms (92.46%), Space: 22.4 MB (21.44%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -5,10 +5,8 @@ class Solution:
 
         for i in range(1, len(intervals)):
             if intervals[i][0] <= newList[-1][1]:
-                newList[-1][0] = min(intervals[i][0], newList[-1][0])
                 newList[-1][1] = max(intervals[i][1], newList[-1][1])
             else:
                 newList.append(intervals[i])
-
         return newList 
         
