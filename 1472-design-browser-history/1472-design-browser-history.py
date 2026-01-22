bb45556a3122c3d711c9765662be00fb4c336803
Karl <93551434+Karl-Fernandes@users.noreply.github.com>
2026-01-22 15:50:52 +0000

    Time: 47 ms (40.47%), Space: 22.2 MB (10.12%) - LeetHub

---
+++ 1472-design-browser-history/1472-design-browser-history.py
@@ -0,0 +1,37 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+        self.prev = None
+
+class BrowserHistory:
+
+    def __init__(self, homepage: str):
+        start_node = Node(homepage)
+        self.current = start_node
+        
+
+    def visit(self, url: str) -> None:
+        new_node = Node(url)
+
+        self.current.next, new_node.prev = new_node, self.current
+        self.current = self.current.next
+
+    def back(self, steps: int) -> str:
+        while steps > 0 and self.current.prev:
+            self.current = self.current.prev
+            steps -= 1
+        return self.current.val  
+
+    def forward(self, steps: int) -> str:
+        while steps > 0 and self.current.next:
+            self.current = self.current.next
+            steps -= 1
+        return self.current.val  
+
+
+# Your BrowserHistory object will be instantiated and called as such:
+# obj = BrowserHistory(homepage)
+# obj.visit(url)
+# param_2 = obj.back(steps)
+# param_3 = obj.forward(steps)
