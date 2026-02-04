76f4b6d5d1db1e6be7888f4b53d5a9f96a921b2b
Karl <93551434+Karl-Fernandes@users.noreply.github.com>
2026-02-04 13:44:55 +0000

    Time: 51 ms (89.88%), Space: 22.6 MB (22.72%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -3,29 +3,37 @@ class Solution:
         if endWord not in wordList:
             return 0
         
-        nei = defaultdict(list)
-        wordList.append(beginWord)
+        if beginWord == endWord:
+            return 1
+        
+
+        graph = defaultdict(list)
         for word in wordList:
-            for j in range(len(word)):
-                pattern = word[:j] + "*" + word[j + 1:]
-                nei[pattern].append(word)
+            for i in range(len(word)):
+                pattern = word[:i] + "*" + word[i+1:]
+                graph[pattern].append(word)
+        
 
-        visited = set([beginWord])
         q = deque([beginWord])
+        visited = set()
+        visited.add(beginWord)
+
         res = 1
-
         while q:
-            for _ in range(len(q)): 
-                current = q.popleft()
-                if current == endWord:
+            for _ in range(len(q)):
+                curr = q.popleft()
+                if curr == endWord:
                     return res
+                
 
-                for j in range(len(current)):
-                    pattern = current[:j] + "*" + current[j + 1:]
-                    for neiWord in nei[pattern]:
-                        if neiWord not in visited:
-                            visited.add(neiWord)
-                            q.append(neiWord)
+                for i in range(len(curr)):
+                    pattern = curr[:i] + "*" + curr[i+1:]
+                    for nei in graph[pattern]:
+                        if nei not in visited:
+                            q.append(nei)
+                            visited.add(nei)        
             res += 1
         
         return 0
+        
+        
